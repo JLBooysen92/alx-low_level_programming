ac883b4f4a8095f90619ba51ffe93808a637fc00
Joe Booysen <Booysen101.joe@gmail.com>
2023-02-16 04:47:38 -0800

    First attempt for puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like builing a multilingual puzzle");
+return (0);
+}
+
